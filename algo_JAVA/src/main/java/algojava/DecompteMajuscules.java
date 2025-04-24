d31f635fdb6595cab83965eff3aabdb448898571
unknown <floallheilly@gmail.com>
2025-04-24 16:58:33 +0200

    Décompte du nombre de majuscules dans une phrase

---
+++ algo_JAVA/src/main/java/algojava/DecompteMajuscules.java
@@ -0,0 +1,35 @@
+package algojava;
+
+import java.util.Scanner;
+
+public class DecompteMajuscules {
+    public static void main(String[] args) {
+
+        // Scanner afin de permettre à l'utilisateur de rentrer une phrase
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ecrivez votre phrase : ");
+        String phrase = scanner.nextLine();
+        scanner.close();
+
+        int compteurMajuscules = 0;
+
+        for (int i = 0; i < phrase.length(); i++) {
+            char majuscule = phrase.charAt(i);
+            if (isMajuscule(majuscule)) {
+                compteurMajuscules++;
+            }
+        }
+
+        System.out.println("Il y a " + compteurMajuscules + " dans la phrase.");
+    }
+
+    /**
+     * 
+     * @param majuscule // Détermine si le caractère est une majuscule ou non
+     * @return // Retourne les caractères en majuscule
+     */
+    public static boolean isMajuscule(char majuscule) {
+        return Character.isUpperCase(majuscule);
+    }
+
+}
