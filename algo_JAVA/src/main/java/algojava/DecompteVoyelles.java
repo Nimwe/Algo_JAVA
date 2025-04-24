3d9153162f86595ab20136935cd47c18d4fd582f
unknown <floallheilly@gmail.com>
2025-04-24 16:34:33 +0200

    Décompte du nombre de voyelles dans une phrase

--- algo_JAVA/src/main/java/algojava/DecompteVoyelles.java
+++ algo_JAVA/src/main/java/algojava/DecompteVoyelles.java
@@ -1,5 +1,39 @@
 package algojava;
 
+import java.text.Normalizer;
+import java.util.Scanner;
+
 public class DecompteVoyelles {
-    
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ecrivez votre phrase : ");
+        String phrase = scanner.nextLine();
+        scanner.close();
+
+        int compteurVoyelles = 0;
+
+        for (int i = 0; i < phrase.length(); i++) {
+            char voyelle = phrase.charAt(i);
+            if (isVoyelle(voyelle)) {
+                compteurVoyelles++;
+            }
+        }
+        System.out.println("Il y a " + compteurVoyelles + " de voyelles dans la phrase.");
+    }
+
+    public static boolean isVoyelle(char voyelle) {
+        String sansAccent = Normalizer.normalize(String.valueOf(voyelle), Normalizer.Form.NFD)
+                .replaceAll("\\p{M}", "").toLowerCase();
+        char minuscule = sansAccent.charAt(0);
+
+        return minuscule == 'a' ||
+                minuscule == 'e' ||
+                minuscule == 'i' ||
+                minuscule == 'o' ||
+                minuscule == 'u' ||
+                minuscule == 'y';
+
+    }
 }
