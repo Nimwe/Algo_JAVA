8774a60bd48f5a4dd5160b884a3a9ed155e5de3c
unknown <floallheilly@gmail.com>
2025-04-25 09:58:09 +0200

    Miroir d'une chaine de caractères

---
+++ algo_JAVA/src/main/java/algojava/MiroirString.java
@@ -0,0 +1,33 @@
+package algojava;
+
+import java.util.Scanner;
+
+public class MiroirString {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Veuillez entrer votre phrase ici : ");
+        String originale = scanner.nextLine();
+        scanner.close();
+
+        String miroir = inverse(originale);
+        System.out.println("Votre phrase inversées est : " + miroir);
+    }
+
+    /**
+     * Fonction miroir pour parcourir la phrase originale à rebours
+     * 
+     * @param originale Entrée utilisateur via le scanner
+     * @return L'entrée originale parcourue à rebour pour inverser le sens
+     *         d'écriture
+     */
+    public static String inverse(String originale) {
+        StringBuilder resultat = new StringBuilder();
+
+        // Parcours de la string à l'envers
+        for (int i = originale.length() - 1; i >= 0; i--) {
+            resultat.append(originale.charAt(i));
+        }
+        return resultat.toString();
+    }
+}
